a741bf7e12b809084970641e161c322a6bfa076e
igrmm <igrmmdev@gmail.com>
2022-02-05 13:59:37 -0400

    first commit

---
+++ android/src/com/igrmm/termoinfinito/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.igrmm.termoinfinito;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.igrmm.termoinfinito.TermoInfinito;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TermoInfinito(), config);
+	}
+}
